aebd13b9f7689ddaa6b0517ef729b4ab15f7f55d
Rish <>
2024-12-27 13:35:25 -0800

    cleaned job description list and work-experience
    also updated skills and the icons

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function switchImage() {
         secondImage.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const thumbnails = document.querySelectorAll('.thumbnail');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('mouseover', function() {
+            const fullsizeImage = document.createElement('img');
+            fullsizeImage.src = thumbnail.getAttribute('data-fullsize');
+            fullsizeImage.classList.add('fullsize-image');
+            thumbnail.parentNode.appendChild(fullsizeImage);
+        });
+
+        thumbnail.addEventListener('mouseout', function() {
+            const fullsizeImage = thumbnail.parentNode.querySelector('.fullsize-image');
+            if (fullsizeImage) {
+                fullsizeImage.remove();
+            }
+        });
+    });
+});
